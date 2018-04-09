e8727ddc5a16f28ae172adc1a2d59507b187c2e6
EarthenSkyLaptop <earthenskymath@gmail.com>
2018-04-09 09:39:18 -0700

    environment setup

---
+++ main.lua
@@ -0,0 +1,14 @@
+-- The initialize function.
+function love.load()
+    print "start"
+end
+
+-- The draw function.
+function love.draw()
+
+end
+
+-- The update function.
+function love.update(dt)
+
+end
